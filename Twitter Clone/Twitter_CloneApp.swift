f0059070646b84a4349f5288afc269df568d9d11
Imran Abdullah <nagatoyahiko856@gmail.com>
2023-08-20 23:24:36 +0300

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by Imran Abdullah on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
