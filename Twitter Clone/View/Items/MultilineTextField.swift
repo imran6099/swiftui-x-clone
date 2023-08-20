edab078588312a1042c4f8c2f37532ffae790488
Imran Abdullah <nagatoyahiko856@gmail.com>
2023-08-21 01:02:15 +0300

    This a Multilinetextfield component, it combines UIKit and SwiftUI, since SwiftUI don't allow us to customize the text field

--- Twitter Clone/View/Items/MultilineTextField.swift
+++ Twitter Clone/View/Items/MultilineTextField.swift
@@ -7,14 +7,41 @@
 
 import SwiftUI
 
-struct MultilineTextField: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MultilineTextField: UIViewRepresentable {
+    func makeCoordinator() -> MultilineTextField.Coordinator {
+        return MultilineTextField.Coordinator(parent1: self)
     }
-}
-
-struct MultilineTextField_Previews: PreviewProvider {
-    static var previews: some View {
-        MultilineTextField()
+    
+    @Binding var text: String
+    
+    func makeUIView(context: Context) -> some UIView {
+        let text = UITextView()
+        text.isEditable = true
+        text.text = "Type Something"
+        text.isUserInteractionEnabled = true
+        text.textColor = .gray
+        text.font = .systemFont(ofSize: 20)
+        text.delegate = context.coordinator
+        return text
+    }
+    
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: MultilineTextField
+        
+        init(parent1: MultilineTextField){
+            parent = parent1
+        }
+        func textViewDidBeginEditing(_ textView: UITextView) {
+            textView.text = ""
+            textView.textColor = .black
+        }
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
     }
 }
